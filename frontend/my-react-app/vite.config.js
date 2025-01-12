9be2bd06838ab9343b683ae704e6188d1a35d472
Tegveer Bhatti <tegveerbhatti@gmail.com>
2025-01-12 12:06:14 -0500

    Redirects file for netlify

--- frontend/my-react-app/vite.config.js
+++ frontend/my-react-app/vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [
+    {
+      name: 'copy-redirects',
+      buildStart() {
+        this.emitFile({
+          type: 'asset',
+          fileName: '_redirects',
+          source: '/*    /index.html   200',
+        });
+      },
+    },
+  ],
+});
