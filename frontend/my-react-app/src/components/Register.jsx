b40b05c4f182d0cff7d4813fecec19bba598c9fa
Tegveer Bhatti <tegveerbhatti@gmail.com>
2025-01-15 15:47:57 -0500

    Remove register w google button

--- frontend/my-react-app/src/components/Register.jsx
+++ frontend/my-react-app/src/components/Register.jsx
@@ -38,7 +38,7 @@ function Register() {
           <div className="max-w-md w-full bg-white p-8 border border-gray-300 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
             <p className="text-center mb-6">Hi, Welcome to Crumbtrackr 👋</p>
-            <button onClick={handleGoogleLogin} className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mb-4">
+            {/* <button onClick={handleGoogleLogin} className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mb-4">
             <img src="https://img.icons8.com/color/16/000000/google-logo.png" alt="Google" className="mr-2"/>
             Sign up with Google
             </button>
@@ -46,7 +46,7 @@ function Register() {
               <div className="border-t border-gray-300 w-1/5"></div>
               <p className="text-gray-500 mx-4">or</p>
               <div className="border-t border-gray-300 w-1/5"></div>
-            </div>
+            </div> */}
             <form onSubmit={handleSubmit}>
               <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700" >Email</label>
